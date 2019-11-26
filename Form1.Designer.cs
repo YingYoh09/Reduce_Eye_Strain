279c4d85cf6360994503597786455423b2b00207
Ying <yingyoh09@gmail.com>
2019-11-26 10:42:37 +0700

    almost done (chua xong vu close)

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace _20_20_20_Rule_Eyestrain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cycleTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BreakTimeTextBox = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.PauseBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.CycleCheckBox = new System.Windows.Forms.CheckBox();
+            this.MsgBox = new System.Windows.Forms.ToolTip(this.components);
+            this.BreakTimeCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cycleTextBox
+            // 
+            this.cycleTextBox.Location = new System.Drawing.Point(97, 26);
+            this.cycleTextBox.Name = "cycleTextBox";
+            this.cycleTextBox.Size = new System.Drawing.Size(40, 22);
+            this.cycleTextBox.TabIndex = 0;
+            this.cycleTextBox.Text = "20";
+            this.cycleTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(143, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Minutes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cycle";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Break time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(143, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Seconds";
+            // 
+            // BreakTimeTextBox
+            // 
+            this.BreakTimeTextBox.Location = new System.Drawing.Point(97, 74);
+            this.BreakTimeTextBox.Name = "BreakTimeTextBox";
+            this.BreakTimeTextBox.Size = new System.Drawing.Size(40, 22);
+            this.BreakTimeTextBox.TabIndex = 3;
+            this.BreakTimeTextBox.Text = "20";
+            this.BreakTimeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(66, 119);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(177, 33);
+            this.StartBtn.TabIndex = 6;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // PauseBtn
+            // 
+            this.PauseBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PauseBtn.Location = new System.Drawing.Point(259, 21);
+            this.PauseBtn.Name = "PauseBtn";
+            this.PauseBtn.Size = new System.Drawing.Size(65, 33);
+            this.PauseBtn.TabIndex = 7;
+            this.PauseBtn.Text = "Pause";
+            this.PauseBtn.UseVisualStyleBackColor = true;
+            this.PauseBtn.Click += new System.EventHandler(this.PauseBtn_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitBtn.Location = new System.Drawing.Point(259, 119);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(65, 33);
+            this.ExitBtn.TabIndex = 8;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(24, 17);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Fb";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Location = new System.Drawing.Point(8, 127);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(44, 17);
+            this.TimeLabel.TabIndex = 10;
+            this.TimeLabel.Text = "20 : 0";
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StopBtn.Location = new System.Drawing.Point(259, 69);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(65, 33);
+            this.StopBtn.TabIndex = 11;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // CycleCheckBox
+            // 
+            this.CycleCheckBox.AutoSize = true;
+            this.CycleCheckBox.Checked = true;
+            this.CycleCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CycleCheckBox.Location = new System.Drawing.Point(225, 29);
+            this.CycleCheckBox.Name = "CycleCheckBox";
+            this.CycleCheckBox.Size = new System.Drawing.Size(18, 17);
+            this.CycleCheckBox.TabIndex = 12;
+            this.MsgBox.SetToolTip(this.CycleCheckBox, "Show MsgBox each cycle");
+            this.CycleCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // BreakTimeCheckBox
+            // 
+            this.BreakTimeCheckBox.AutoSize = true;
+            this.BreakTimeCheckBox.Checked = true;
+            this.BreakTimeCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.BreakTimeCheckBox.Location = new System.Drawing.Point(225, 77);
+            this.BreakTimeCheckBox.Name = "BreakTimeCheckBox";
+            this.BreakTimeCheckBox.Size = new System.Drawing.Size(18, 17);
+            this.BreakTimeCheckBox.TabIndex = 13;
+            this.MsgBox.SetToolTip(this.BreakTimeCheckBox, "Show MsgBox each break time finished");
+            this.BreakTimeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(336, 167);
+            this.Controls.Add(this.BreakTimeCheckBox);
+            this.Controls.Add(this.CycleCheckBox);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.PauseBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BreakTimeTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cycleTextBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "20-20-20 Rule Eye Strain";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cycleTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox BreakTimeTextBox;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button PauseBtn;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.CheckBox CycleCheckBox;
+        private System.Windows.Forms.ToolTip MsgBox;
+        private System.Windows.Forms.CheckBox BreakTimeCheckBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20_20_20_Rule_Eyestrain
+{
+    public partial class Form1 : Form
+    {
+        private int Counter;
+        private bool isBreak = false;
+        private bool msgBoxShown = false, isExit = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (StartBtn.Text == "Start")
+            {
+                Counter = int.Parse(cycleTextBox.Text)*60;
+            }
+            else
+            {
+                StartBtn.Text = "Start";
+            }
+
+
+            timer1.Enabled = true;
+            StartBtn.Enabled = false;
+            PauseBtn.Enabled = true;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Counter--;
+            TimeLabel.Text = (Counter / 60).ToString() + " : " + (Counter % 60).ToString();
+            if (Counter == 0)
+            {
+                if (!isBreak)
+                {
+                    Counter = int.Parse(BreakTimeTextBox.Text);
+                    if (CycleCheckBox.Checked)
+                    {
+                        timer1.Enabled = false;
+                        if (MessageBox.Show("Nghỉ em ei") == DialogResult.OK)
+                            timer1.Enabled = true;
+                        else timer1.Enabled = true;
+                    }
+                    
+                }
+                else
+                {
+                    if (BreakTimeCheckBox.Checked)
+                    {
+                        timer1.Enabled = false;
+                        if (MessageBox.Show("Làm tiếp nào") == DialogResult.OK)
+                            timer1.Enabled = true;
+                        else timer1.Enabled = true;
+                    }
+                    StartBtn.PerformClick();
+                }
+
+                isBreak = !isBreak;
+            }
+        }
+
+        private void PauseBtn_Click(object sender, EventArgs e)
+        {
+            StartBtn.Text = "Resume";
+
+            timer1.Enabled = false;
+            StartBtn.Enabled = true;
+            PauseBtn.Enabled = false;
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                linkLabel1.LinkVisited = true;
+                System.Diagnostics.Process.Start("https://www.facebook.com/YingYoh2k");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("https://www.facebook.com/YingYoh2k");
+            }
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            StartBtn.Text = "Start";
+            StartBtn.Enabled = true;
+            PauseBtn.Enabled = false;
+            timer1.Enabled = false;
+            TimeLabel.Text = cycleTextBox.Text + " : " + BreakTimeTextBox.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
